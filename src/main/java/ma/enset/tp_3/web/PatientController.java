a6ea02200e4f58501099c4ecd573e310892fe33c
mohammedtlemsani <mohammedtlemsani01@gmail.com>
2024-03-06 20:47:22 +0100

    pagination added

--- src/main/java/ma/enset/tp_3/web/PatientController.java
+++ src/main/java/ma/enset/tp_3/web/PatientController.java
@@ -3,9 +3,13 @@ package ma.enset.tp_3.web;
 import ma.enset.tp_3.entities.Patient;
 import ma.enset.tp_3.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,9 +18,11 @@ public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
     @GetMapping(path = "/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model, @RequestParam(name = "page",defaultValue = "0")int  page, @RequestParam(name = "size",defaultValue = "5")int size) {
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", patients.getContent());
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "patients";
     }
     @GetMapping(path = "/deletePatient")
